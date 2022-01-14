0e704bedda0604496332d1760380c90136c9378c
Marlie Dueck <marlie.dueck@homecarehub.com>
2022-01-13 16:05:11 -0800

    add photo example and documentation plus finish the comonent

--- README.md
+++ README.md
@@ -5,20 +5,30 @@
 npm install
 ```
 
-### Compiles and hot-reloads for development
+## Usage
+Import into your Vue project
 ```
-npm run serve
+import FiveStarInput from 'vue-five-star-input';
+export default { 
+    components: {
+        FiveStarInput,
+    }
+}
+```
+Use in your project
+```
+<five-star-input name="customer-review" value="3" />
 ```
 
-### Compiles and minifies for production
-```
-npm run build
-```
+##  Props:
+`name`: The name of the input field
 
-### Lints and fixes files
-```
-npm run lint
-```
+`value`: (optional) Default value of the five stars (0-5)
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+`required`: (optional. default=false). Boolean whether the input field is required
+
+## Styling:
+Style the stars using the `.star` class. You can change the color and height of the icons.
+
+## Example
+![image of five star input asking for a review](https://github.com/marlieer/vue-five-star-input/blob/[main]/FivestarInput.png?raw=true)
--- src/component.vue
+++ src/component.vue
@@ -1,18 +1,25 @@
 <template>
 <div class="stars">
-    <input :value="rating" :name="name" type="hidden"/>
-    <img v-for="value in values" :key="value" 
-        :src="rating > value - 1 ? filledStar : emptyStar" 
-        height="28px" width="28px" 
-        @click="rating=value" 
-        class="star"/>
+    <input :value="rating" :name="name" type="hidden" :required="required"/>
+    <span v-for="value in values" :key="value"
+        @click="rating=value">
+        <font-awesome-icon class="star" :icon="['fas', 'star']" v-if="rating > value - 1"/>
+        <font-awesome-icon class="star" :icon="['far', 'star']" v-else/>
+    </span>
 </div>
 </template>
 <script>
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faStar as faStarFilled} from '@fortawesome/free-solid-svg-icons'
+import { faStar } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 export default {
-    name: "VueFiveStarInput",
+    name: "FiveStarInput",
+    components: {
+        FontAwesomeIcon
+    },
     props: [
-        'name', 'value'
+        'name', 'value', 'required'
     ],
     data() {
         return {
@@ -21,16 +28,20 @@ export default {
             emptyStar: '',
             values: [1,2,3,4,5]
         }
+    },
+    created() {
+        library.add(faStar, faStarFilled)
     }
 }
 </script>
-<style lang="scss" scoped>
+<style scoped>
     div.radio-buttons, div.labels {
         display: flex;
         justify-content: space-around;
     }
     .star {
         margin: 0 0.3rem;
+        width: unset;
     }
     .star:hover {
         cursor: pointer;
